e11a2a610dd384010e545bb5273d37728f1c82a2
chernuw <gotovkoromanhzby@gmail.com>
2018-02-04 02:32:36 +0300

    rework delete action

--- src/main/java/by/rgotovko/controller/employee/EmployeeDelete.java
+++ src/main/java/by/rgotovko/controller/employee/EmployeeDelete.java
@@ -2,25 +2,24 @@ package by.rgotovko.controller.employee;
 
 import by.rgotovko.dao.DAOException;
 import by.rgotovko.dao.DAOFactory;
-import by.rgotovko.entity.Employee;
+import by.rgotovko.entity.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(name = "EmployeeDelete", urlPatterns = "/EmployeeDelete")
 public class EmployeeDelete extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
-            ServletException, IOException {
+            ServletException{
         try{
-            Integer oldEmployee = Integer.parseInt(request.getParameter("idEmployee"));
-            Employee employee = DAOFactory.getEmployeeDAO().getEmployeeById(oldEmployee);
-            employee.setDepartment(null);
-            employee.setPosition(null);
-            DAOFactory.getEmployeeDAO().deleteEmployee(employee);
+            Integer id = Integer.parseInt(request.getParameter("employeeId"));
+            User user = DAOFactory.getUserDAO().getUserById(id);
+            user.getEmployee().setDepartment(null);
+            user.getEmployee().setPosition(null);
+            DAOFactory.getUserDAO().deleteUser(user);
         } catch (DAOException e) {
             throw new ServletException(e);
         }
